697790c2ce7ec43b01ceb6cc48776782475e2bbf
tognileo <tognileo08@gmail.com>
2026-08-16 22:12:18 -0300

    atualizando projeto

--- Model/classes/database.py
+++ Model/classes/database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-DATABASE_URL = "mysql+pymysql://root:@localhost:3306/plataforma_de_curso"
+DATABASE_URL = "mysql+pymysql://root:@localhost:3306/plataforma_cursos"
 
 engine = create_engine(DATABASE_URL)
 session_local = sessionmaker(autocommit=False,autoflush=False , bind=engine)
